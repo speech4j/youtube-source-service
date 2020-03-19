bbd03de9e41f26a36524cceafea0d0922aa13716
dpavlo <mrscors@gmail.com>
2020-03-19 20:36:53 +0200

    /download endpoint returns .mp3 file now instead of it's location in the file system

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,9 @@
 FROM python:3.8
 
-EXPOSE 5000
-
 ADD requirements.txt requirements.txt
 ADD src src
-ADD resources resources
 
+RUN mkdir /resources
 RUN pip install -r requirements.txt
-ENTRYPOINT python src/youtube-downloader.py
+
+ENTRYPOINT ["python", "src/youtube-downloader.py", "/resources"]
--- src/youtube-downloader.py
+++ src/youtube-downloader.py
@@ -1,12 +1,12 @@
 import os
 import string
 import random
+import sys
 
-from flask import Flask, request
+from flask import Flask, request, send_from_directory
 from pytube import YouTube
 
 from moviepy.editor import VideoFileClip
-
 
 app = Flask(__name__)
 
@@ -22,7 +22,7 @@ def get_extension(filename):
 
 def mp4_to_mp3(input_file_name):
     output_file_dir = input_file_name[:input_file_name.rindex("/")]
-    output_file_name = input_file_name[(input_file_name.rindex("/")+1):].replace("mp4", "mp3")
+    output_file_name = input_file_name[(input_file_name.rindex("/") + 1):].replace("mp4", "mp3")
     video = VideoFileClip(os.path.join(input_file_name))
     video.audio.write_audiofile(os.path.join(output_file_dir, output_file_name))
     os.remove(input_file_name)
@@ -32,19 +32,23 @@ def mp4_to_mp3(input_file_name):
 @app.route('/download')
 def download_youtube_video():
     url = request.args.get('url')
-    local_uri = "/resources"
+    resources_folder = sys.argv[1]
 
     if url.startswith("https://www.youtube.com/watch?v="):
         yt = YouTube(url)
-        video = yt.streams.first().download(local_uri)
+        video = yt.streams.first().download(resources_folder)
 
         new_video_name = random_string(20) + get_extension(video)
-        mp4_file_location = local_uri + "/" + new_video_name
+        mp4_file_location = resources_folder + "/" + new_video_name
         os.rename(video, mp4_file_location)
 
-        mp3_file_location = mp4_to_mp3(mp4_file_location)
+        mp3_file_name = mp4_to_mp3(mp4_file_location)
+        resources_folder += "/"
 
-        return mp3_file_location, 200
+        mp3_file_to_send = send_from_directory(resources_folder, mp3_file_name)
+        os.remove(resources_folder + mp3_file_name)
+
+        return mp3_file_to_send, 200
 
     return "Invalid YouTube link", 400
 
